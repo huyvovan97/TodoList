0170c24b1eb28d2009ee847e020a362f687aaeeb
huyvovan97 <huyvovan97@gmail.com>
2021-01-28 07:50:41 +0700

    move things

---
+++ TodoListBackEnd/src/main/java/com/mysoftware/todolist/TodoListBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mysoftware.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListBackEndApplication.class, args);
+	}
+
+}
---
+++ TodoListBackEnd/src/test/java/com/mysoftware/todolist/TodoListBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysoftware.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
